48601fe58fdda4b6177d0834625808b6427404a4
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-06 17:47:00 +0100

    create template for todo list using functional components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+      
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  );
+}
+
+export default App;
